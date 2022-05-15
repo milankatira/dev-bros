f0cda05d485c9480fcea26afc7d6c0364c50a0a0
milan Katira <84834795+milankatira@users.noreply.github.com>
2022-05-15 17:21:04 +0530

    fix build error

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
+ 
 module.exports = {
-  images: ["localhost", "images.unsplash.com"],
+  reactStrictMode: true,
+  images: {
+    domains: ["localhost", "images.unsplash.com"],
+  },
   env: {
     ENV: process.env.ENV,
   },
-  reactStrictMode: true,
 };
 
- 
