c93c6121583a9a7da2eeddd1835f72028e849c5c
milan Katira <84834795+milankatira@users.noreply.github.com>
2022-06-12 23:06:01 +0530

    addedchages

--- server/utils/jwtToken.js
+++ server/utils/jwtToken.js
@@ -1,5 +1,15 @@
+const cookie = require("cookie");
+
 const sendToken = (user, statusCode, res) => {
   const token = user.getJWTToken();
+  const serialized = cookie.serialize("token", token, {
+    httpOnly: false,
+    secure: process.env.MODE_ENV !== "development",
+    // sameSite: "strict",
+    maxAge: 60 * 60 * 24 * 1, // 1 day
+    path: "/",
+  });
+  res.setHeader("Set-Cookie", serialized);
 
   const options = {
     expires: new Date(Date.now() + 70000000),
