dcf9eb9830ef32fe5c70bbac5b87c71d2148e7d2
milan Katira <84834795+milankatira@users.noreply.github.com>
2022-07-02 11:56:01 +0530

    added infinite scroll

--- frontend/pages/company/exam/index.tsx
+++ frontend/pages/company/exam/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import AddExamModal from "../../../components/admin/common/AddExamModal";
 import HeaderTitleContainer from "../../../components/common/design/HeaderTitleContainer";
 import axios from "axios";
@@ -6,8 +6,7 @@ import useInfiniteScroll from "../../../hook/useInfinateScroll";
 import useBookSearch from "../../../hook/fetchHook";
 import { GetAllExam } from "../../../api/client/exam";
 import Link from "next/link";
-const Index = ({ Data }) => {
-  console.log(Data, "dataa");
+const Index = () => {
   const initialValues = {
     exam_name: "",
     start_time: "",
@@ -23,45 +22,116 @@ const Index = ({ Data }) => {
   const [openAddExamModal, setOpenAddExamModal] = useState(true);
   const [examDataForModal, setExamDataForModal] = useState<any>(initialValues);
   const toggleModal = () => setOpenAddExamModal(!openAddExamModal);
-  const [IsFetching, setIsFetching] = useState(false);
-  const [Page, setPage] = useState(0);
-  const [HasMore, setHasMore] = useState(10);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [books, setBooks] = useState([]);
+  const [hasMore, setHasMore] = useState(false);
 
-    const [pageNumber, setPageNumber] = useState(1);
+  useEffect(() => {
+    setLoading(true);
+    setError(false);
+    let cancel;
+    axios({
+      method: "GET",
+      url: "http://localhost:4000/api/exam",
+      params: { itemsPerPage: 10, offset: (pageNumber - 1) * 10 },
+      cancelToken: new axios.CancelToken((c) => (cancel = c)),
+    })
+      .then((res) => {
+        console.log(res.data.exam, "Ss");
+        setBooks((prevBooks) => {
+          return [
+            ...new Set([...prevBooks, ...res.data.exam.map((b: any) => b)]),
+          ];
+        });
+        setHasMore(res.data.exam.length >= 10);
+        setLoading(false);
+      })
+      .catch((e) => {
+        if (axios.isCancel(e)) return;
+        setError(true);
+      });
+    return () => cancel();
+  }, [pageNumber]);
 
-    // const { books, hasMore, loading, error } = useBookSearch(pageNumber);
+  const observer = useRef();
+  const lastBookElementRef = useCallback(
+    (node) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPageNumber((prevPageNumber) => prevPageNumber + 1);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [loading, hasMore]
+  );
+
   return (
     <div>
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap -m-4">
-            {Data.exam.map((exam: any) => {
-              console.log(exam, "exam");
-              return (
-                <>
-                  <Link href={`exam/${exam._id}`}>
-                    <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-                      <a className="block relative h-48 rounded overflow-hidden">
-                        <img
-                          alt="ecommerce"
-                          className="object-cover object-center w-full h-full block"
-                          src="https://dummyimage.com/428x268"
-                        />
-                      </a>
-                      <div className="mt-4">
-                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                          {exam.exam_type}
-                        </h3>
-                        <h2 className="text-gray-900 title-font text-lg font-medium">
-                          {exam.exam_name}
-                        </h2>
-                        <p className="mt-1">{exam.description}</p>
-                      </div>
-                    </div>
-                  </Link>
-                </>
-              );
-            })}
+            {books &&
+              books.length &&
+              books?.map((book, index) => {
+                if (books.length === index + 1) {
+                  return (
+
+                      <Link href={`exam/${book._id}`}>
+                        <div className="lg:w-1/4 md:w-1/2 p-4 w-full" ref={lastBookElementRef} key={book}>
+                          <a className="block relative h-48 rounded overflow-hidden">
+                            <img
+                              alt="ecommerce"
+                              className="object-cover object-center w-full h-full block"
+                              src="https://dummyimage.com/428x268"
+                            />
+                          </a>
+                          <div className="mt-4">
+                            <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                              {book.exam_type}
+                            </h3>
+                            <h2 className="text-gray-900 title-font text-lg font-medium">
+                              {book.exam_name}
+                            </h2>
+                            <p className="mt-1">{book.description}</p>
+                          </div>
+                        </div>
+                      </Link>
+                  );
+                } else {
+                  return (
+                      <Link href={`exam/${book._id}`}>
+                        <div
+                          className="lg:w-1/4 md:w-1/2 p-4 w-full"
+                          key={book}
+                        >
+                          <a className="block relative h-48 rounded overflow-hidden">
+                            <img
+                              alt="ecommerce"
+                              className="object-cover object-center w-full h-full block"
+                              src="https://dummyimage.com/428x268"
+                            />
+                          </a>
+                          <div className="mt-4">
+                            <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                              {book.exam_type}
+                            </h3>
+                            <h2 className="text-gray-900 title-font text-lg font-medium">
+                              {book.exam_name}
+                            </h2>
+                            <p className="mt-1">{book.description}</p>
+                          </div>
+                        </div>
+                      </Link>
+                  );
+                }
+              })}
+            <div>{loading && "Loading..."}</div>
+            <div>{error && "Error"}</div>
           </div>
         </div>
       </section>
@@ -71,15 +141,3 @@ const Index = ({ Data }) => {
 
 export default Index;
 
-export const getServerSideProps = async ({ req }) => {
-  const res = await axios.get("http://localhost:4000/api/exam", {
-    withCredentials: true,
-    headers: {
-      "Access-Control-Allow-Credentials": true,
-      Cookie: req?.headers?.cookie,
-    },
-  });
-
-  const data = await res.data;
-  return { props: { Data: data } };
-};
