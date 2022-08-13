038fa916bed36b59319ab580e48578eb70d29ae9
milan Katira <84834795+milankatira@users.noreply.github.com>
2022-08-13 16:11:42 +0530

    added changes

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,4 +1,4 @@
-/* eslint-disable react/react-in-jsx-scope */
+import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
